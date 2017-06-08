c5380abd05bb960d5d63d61f9516633c0096aa91
gabrijela gavric and oona roe <oona@>
2017-06-07 19:31:21 -0700

    made page showing utalzing toggle and other methods. and funcitons

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
+  });
+
+});
